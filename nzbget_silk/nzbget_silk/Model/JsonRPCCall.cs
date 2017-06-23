8eb287a4d1ee0dae640845779a7f16c60566b240
Jens Marchewka <im.naice@googlemail.com>
2017-06-23 16:06:21 +0200

    Commented

--- nzbget_silk/nzbget_silk/Model/JsonRPCCall.cs
+++ nzbget_silk/nzbget_silk/Model/JsonRPCCall.cs
@@ -10,7 +10,9 @@ namespace nzbget_silk.Model
     {
         public string jsonrpc { get; private set; } = "2.0";
         public string method { get; set; }
-        // No named params!
+        /// <summary>
+        /// As of the current version 22.06.2017 no named Params are allowed.
+        /// </summary>
         public object[] @params { get; set; }
         public string id { get; private set; } = Guid.NewGuid().ToString().Replace("-", "");
 
